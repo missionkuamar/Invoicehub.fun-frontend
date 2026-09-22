2881a671db61a3de19741cc0e42205d2b0b6f02e
mission kumar <missionk935@gmail.com>
2026-09-22 20:22:39 +0530

    add affiliateRedirect link

--- src/App.jsx
+++ src/App.jsx
@@ -53,7 +53,7 @@ import RefundPolicy from './components/homepage/contact/RefundPolicy';
 const AffiliateRedirect = () => {
   const { slug } = useParams();
   useEffect(() => {
-    window.location.href = `http://localhost:5000/r/${slug}${window.location.search}`;
+    window.location.href = `https://invoicehub-fun-backend.onrender.com/r/${slug}${window.location.search}`;
   }, [slug]);
   return (
     <div className="flex justify-center items-center h-64">
