e669ff98f9bac1369ab9fe6c8ec7326d54105e75
mission kumar <missionk935@gmail.com>
2026-09-22 11:31:06 +0530

    fix google oauth api url

--- src/services/api.js
+++ src/services/api.js
@@ -4,8 +4,8 @@ import toast from 'react-hot-toast';
 
 // Get API URL from environment or use default
 //const API_URL = import.meta.env.VITE_API_URL_API || 'http://localhost:5000/api';
-//const API_URL = https://invoice-saas-with-email.onrender.com
-const API_URL = import.meta.env.VITE_API_URL_API || "https://invoicehub-fun-backend.onrender.com/api";
+const API_URL = 'https://invoicehub-fun-backend.onrender.com/api';
+//const API_URL = import.meta.env.VITE_API_URL_API || "https://invoicehub-fun-backend.onrender.com/api";
 // Create axios instance
 const api = axios.create({
   baseURL: API_URL,
